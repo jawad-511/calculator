09565b944c056be988a05c28d9840e8d857fa2b7
Jawad Alotibi <s444003256@st.uqu.edu.sa>
2024-07-07 16:40:53 +0300

    Cwntwerd white rectangile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
     <div class="flex justify-center items-center bg-white h-[30rem] w-[20rem] rounded-3xl" >
         <div class="flex justify-center items-center bg-black h-[5rem] w-[5rem]"></div>
-
+             
     </div>
 
  
