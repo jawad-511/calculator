1beceb3beb09f0b7189a960040d07764742db384
Jawad Alotibi <s444003256@st.uqu.edu.sa>
2024-07-16 02:27:16 +0300

    added shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,41 @@
     <title>Page</title>
 </svelte:head>
 
-    <div class="flex justify-center items-center bg-white h-[30rem] w-[20rem] rounded-3xl" >
-        <div class="flex justify-center items-center bg-black h-[5rem] w-[5rem]"></div>
-             
+    <div class="bg-white min-h-[30rem] w-[20rem] rounded-3xl grid grid-cols-4 gap-1 p-6 font-semibold text-3xl shadow-2xl">
+        <div class="
+            bg-blue-600
+            rounded-full
+            col-span-4
+            h-12
+            flex items-center
+            px-4
+            mb-2 
+            text-white
+            text-lg 
+            ">
+
+            12345
+        </div>
+       <button class="bg-slate-100 rounded-full w-14 h-14  text-gray-600">%</button>
+       <button class="bg-slate-100 rounded-full w-14 h-14  text-gray-600">&radic;</button>
+       <button class="bg-slate-100 rounded-full w-14 h-14  text-gray-600">CE</button>
+       <button class="bg-slate-950 rounded-full w-14 h-14 text-white">C</button>
+       <button class="text-gray-600">7</button>
+       <button class="text-gray-600">8</button>
+       <button class="text-gray-600">9</button>
+       <button class="bg-red-500 rounded-full w-14 h-14 text-white">-</button>
+       <button class="text-gray-600">4</button>
+       <button class="text-gray-600">5</button>
+       <button class="text-gray-600">6</button>
+       <button class="bg-blue-700 rounded-full w-14 h-14 text-white">\</button> 
+       <button class="text-gray-600">1</button>
+       <button class="text-gray-600">2</button>
+       <button class="text-gray-600">3</button>
+       <button class="bg-yellow-400 rounded-full w-14 h-14 text-white">*</button>
+       <button class="text-gray-600">.</button>
+       <button class="text-gray-600">0</button>
+       <button class="bg-slate-100 rounded-full w-14 h-14 text-gray-600">=</button>
+       <button class="bg-green-500 rounded-full w-14 h-14 text-white">+</button>
     </div>
 
  
